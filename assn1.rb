8e2e42b8ffcfb7771b04525c1d721bd6c9295824
Atreyee Paul <66584524+atreyee-99@users.noreply.github.com>
2022-04-04 14:46:57 +0530

    Add files via upload

---
+++ assn1.rb
@@ -0,0 +1,27 @@
+class Main
+    def self.main()
+        n = 6
+        # given size
+        i = 0
+        while (i < n)
+            k = 0
+            while (k < i)
+                print("\\\\")
+                k += 1
+            end
+            l = 0
+            while (l < 2 * n - 1 - 2 * i)
+                print("!!")
+                l += 1
+            end
+            k = 0
+            while (k < i)
+                print("\\\\")
+                k += 1
+            end
+            print("\n")
+            i += 1
+        end
+    end
+end
+Main.main()
