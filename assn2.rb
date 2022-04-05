c91bc74241ff9c9b0d5650046c827c5727cbf52e
Atreyee Paul <66584524+atreyee-99@users.noreply.github.com>
2022-04-05 14:02:59 +0530

    Update assn2.rb

--- assn2.rb
+++ assn2.rb
@@ -1,50 +1,105 @@
-class Main
-    def self.main()
-        grades = Hash.new
+class HashImplementation
+    def operation
+        @dict = Hash.new
+        @dict = { "monotonous" => "dull, tedious, and repetitious; lacking in variety and interest." , 
+                "lewd" => "crude and offensive.",
+                "seclusion" => "the state of being private and away from other people.",
+                "veritable" => "used for emphasis, often to qualify a metaphor.",
+                "bedlam" => "a scene of uproar and confusion.",
+                "chirrup" => "(of a person) say something in a high-pitched voice.",
+                "frivolous" => "not having any serious purpose or value.",
+                "vexatious" => "causing or tending to cause annoyance, frustration, or worry.",
+                "infuriating" => "making one extremely angry and impatient.",
+                "shroud" => "a thing that envelops or obscures something.",
+                "pallor" => "an unhealthy pale appearance.",
+                "cremate" => "dispose of (a dead person's body) by burning it to ashes, typically after a funeral ceremony.",
+                "shoo" => "make (a person or animal) go away by waving one's arms at them, saying ‘shoo’, or otherwise acting in a discouraging manner."
+                }
+        puts @dict.sort
 
-        #1. Adding values
-        grades["Bob"] = 82 
-        grades["Jim"] = 94
-        grades["Billy"] = 58
 
-        #Retreiving value by key
-        puts grades["Bob"] 
-
-        #Accessing values
-        grades.each do|name,grade|
-            puts "#{name}: #{grade}"
+        def search(word)
+            if @dict.key?(word)==true
+                a=@dict.fetch(word)
+                puts "Word found  with meaning #{a}"
+                puts "\n\n"
+            else
+                puts "Word  not found in the dictionary \n\n"
             end
-
-        grade = { "Bob" => 82,
-        "Jim" => 94,
-        "Billie" => 58
-        }
-
-        puts grade["Billie"]
-
-        grade["Joel"]=44
-        puts grade["Joel"]
-
-
-        #2. Removing values from Hash
-        grade.delete("Billie")
-
-        grade.each do|name,grade|
-            puts "#{name}: #{grade}"
+        end
+        def add(word, meaning)
+            @dict[word]=meaning
+            puts "\n"
+            puts @dict.sort
+            puts "\n\n"
         end
 
-    
-        #3. Search for an element in a Hash
-        puts grade["Joel"]   #This searches for a value using the key and then displays it
-        puts grade["Ravi"]   #This displayes blank since no value is present in the hash with this key
-
-
-        #4. Modify an element in the Hash
-        grade["Joel"]= 89
-
-        grade.each do|name,grade|
-            puts "#{name}: #{grade}"
+        def update(word, meaning)
+            if @dict.key?(word)==true
+                @dict[word]=meaning
+                puts "Meaning updated "
+                # @dict.update(word,meaning)
+                puts @dict.sort
+                puts "\n\n"
+            else
+                puts "Word  not found in the dictionary \n\n"
+            end
         end
+
+        def del(word)
+            if @dict.key?(word)==true
+                @dict.delete(word)
+                puts "Word Deleted successfully"
+                puts @dict.sort
+            else
+                puts "Word not found in the dictionary"
+            end
+        end
+
+        x=0
+        while x!=6
+            puts "Choose from the menu below"
+            puts "1. Show contents of dictionary"
+            puts "2. Search for a word"
+            puts "3. Add a word "
+            puts "4. Update a word"
+            puts "5. Delete a word"
+            puts "6. Exit the application"
+            x=gets.to_i
+            case x
+            when 1
+                @dict.each do|word,meaning|
+                    puts "#{word}: #{meaning}"
+                end
+            when 2
+                puts "Enter the word to be searched for"
+                word=gets.strip
+                search(word)
+            when 3
+                puts "Enter the word to be added"
+                word=gets.strip
+                puts "Enter the meaning of the word #{word}"
+                meaning=gets.strip
+                add(word,meaning)
+            when 4
+                puts "Enter the word to update the meaning "
+                word=gets.strip
+                puts "Enter the updated meaning of the word #{word}"
+                updated_meaning=gets.strip
+                update(word,meaning)
+
+            when 5
+                puts "Enter the word to be deleted from the dictionary"
+                word = gets.strip
+                del(word)
+            when 6
+                puts " End of the Application"
+            else
+                puts "Please enter a valid choice "
+            end
+        end
+        puts x 
     end
 end
-Main.main()
+obj=HashImplementation.new
+obj.operation
